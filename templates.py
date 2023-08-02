237512741f09f4c70cff4874857b807b3b071367
frantastic7 <slijepcevicfran@gmail.com>
2023-08-02 04:35:35 +0200

    Templates

---
+++ templates.py
@@ -0,0 +1,83 @@
+LATEX_1 = r"""\documentclass{article}
+\usepackage[utf8]{inputenc}
+
+\title{Basic Article}
+\author{Author Name}
+\date{Year Month Day}
+
+\begin{document}
+
+\maketitle
+
+\section{Introduction}
+
+Here is where you can write your introduction.
+
+\section{Main Body}
+
+Here is where you can write the main body of your article.
+
+\end{document}
+"""
+
+
+LATEX_2 = r"""\documentclass{article}
+\usepackage[utf8]{inputenc}
+\usepackage{amsmath}
+\usepackage{graphicx}
+
+\title{Mathematics and Physics}
+\author{Author Name}
+\date{Year Month Day}
+
+\begin{document}
+
+\maketitle
+
+\section{Equations}
+
+Insert your equation here. Here is an example of how to insert an equation:
+
+\begin{equation*}
+a^2 + b^2 = c^2
+\end{equation*}
+
+\section{Matrices}
+
+Insert your matrix here. Here is an example of how to insert a matrix:
+
+\begin{equation*}
+\begin{bmatrix}
+1 & 2 & 3\\
+4 & 5 & 6\\
+7 & 8 & 9
+\end{bmatrix}
+\end{equation*}
+
+\section{Differential Equations}
+
+Insert your differential equation here. Here is an example of how to insert a differential equation:
+
+\begin{equation*}
+\frac{d^2y}{dx^2} + \frac{dy}{dx} + y = 0
+\end{equation*}
+
+\end{document}
+"""
+
+
+
+HTML_5 = r"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+"""
+
